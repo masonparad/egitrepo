260c5035203b593563d84faf2df127f6e21afd88
Mason <Mason@DESKTOP-IAJ7DLL>
2021-03-22 22:25:10 -0500

    new feature added

--- Egittest/src/ello.java
+++ Egittest/src/ello.java
@@ -4,6 +4,7 @@ public class ello {
 	public static void main(String[] args) {
 		System.out.println("ello govna");
 		System.out.println("new line has been added");
+		System.out.println("This is a new feature");
 	}
 
 }
